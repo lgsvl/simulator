e53aba16f8f921411768f655248722da029bf91a
ruudy.liu <ruudy.liu@lge.com>
2018-07-09 21:39:38 -0700

    bake build version into standalone player

---
+++ Assets/Scripts/BuildInfo.cs
@@ -0,0 +1,11 @@
+/**
+ * Copyright (c) 2018 LG Electronics, Inc.
+ *
+ * This software contains code licensed as described in LICENSE.
+ *
+ */
+
+public class BuildInfo
+{
+    public const string buildVersion = "${developer-build}"; 
+}

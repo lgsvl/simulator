f96aee45945c49faadfc56fcfedb8243ee0a85eb
ruudy.liu <ruudy.liu@lge.com>
2018-07-11 18:09:51 -0700

    support signal light, pole in vector map tool

---
+++ Assets/Scripts/VectorMapPole.cs
@@ -0,0 +1,15 @@
+﻿/**
+ * Copyright (c) 2018 LG Electronics, Inc.
+ *
+ * This software contains code licensed as described in LICENSE.
+ *
+ */
+
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VectorMapPole : MonoBehaviour
+{
+    public List<VectorMapSignalLight> signalLights;
+    public float length = 13.5f;
+}
---
+++ Assets/Scripts/VectorMapPoleEditor.cs
@@ -0,0 +1,28 @@
+﻿/**
+ * Copyright (c) 2018 LG Electronics, Inc.
+ *
+ * This software contains code licensed as described in LICENSE.
+ *
+ */
+
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(VectorMapPole)), CanEditMultipleObjects]
+public class VectorMapPoleEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+    }
+
+    protected void OnSceneGUI()
+    {
+        VectorMapPole pole = (VectorMapPole)target;
+        Vector3 pos = pole.transform.position;
+        Vector3 dir = pole.transform.forward;
+        float length = pole.length;
+
+        VectorMap.Draw.DrawArrowForDebug(pos, pos + dir * length, Color.white, VectorMapTool.ARROWSIZE);
+    }
+}

f515260eae28922e7f31bc35907e2a1c901dcdf8
Andrey Kozhanov <andrey.kozhanov@lge.com>
2018-07-13 14:09:58 -0700

    add GPS publishing for Apollo

--- Assets/Scripts/Ros/PbMsgs.cs
+++ Assets/Scripts/Ros/PbMsgs.cs
@@ -29,4 +29,34 @@
         public Point3D linear_acceleration;
         public Point3D angular_velocity;
     }
+
+    [MessageType("pb_msgs/GnssBestPose")]
+    public struct GnssBestPose
+    {
+        public ApolloHeader header;
+        public double measurement_time;
+        public int sol_status;
+        public int sol_type;
+
+        public double latitude;  // in degrees
+        public double longitude;  // in degrees
+        public double height_msl;  // height above mean sea level in meters
+        public float undulation;  // undulation = height_wgs84 - height_msl
+        public int datum_id;  // datum id number
+        public float latitude_std_dev;  // latitude standard deviation (m)
+        public float longitude_std_dev;  // longitude standard deviation (m)
+        public float height_std_dev;  // height standard deviation (m)
+        public string base_station_id;  // base station id
+        public float differential_age;  // differential position age (sec)
+        public float solution_age;  // solution age (sec)
+        public int num_sats_tracked;  // number of satellites tracked
+        public int num_sats_in_solution;  // number of satellites used in solution
+        public int num_sats_l1;  // number of L1/E1/B1 satellites used in solution
+        public int num_sats_multi;  // number of multi-frequency satellites used in solution
+        public int reserved;  // reserved
+        public int extended_solution_status;  // extended solution status - OEMV and
+                                                    // greater only
+        public int galileo_beidou_used_mask;
+        public int gps_glonass_used_mask;
+    }
 }

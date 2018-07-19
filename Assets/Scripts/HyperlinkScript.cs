4e9bbcf34fef1178e76efc36d77e3e8a9fd4d215
ruudy.liu <ruudy.liu@lge.com>
2018-07-19 12:20:00 -0700

    make repo and license hyperlink in UI

---
+++ Assets/Scripts/HyperlinkScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HyperlinkScript : MonoBehaviour
+{
+    public string hyperlink;
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(() =>
+        {
+            Application.OpenURL(hyperlink);
+        });
+    }
+}

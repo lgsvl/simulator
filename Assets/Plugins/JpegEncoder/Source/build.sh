fec5435d3ddb8f9bd93d844ded3f57916c7a2348
Martins Mozeiko <martins.mozeiko@lge.com>
2018-08-31 13:09:21 -0700

    Using libjpeg-turbo for faster jpeg encoding
    For 1080p images encoding performance is increased ~10x.

--- Assets/Plugins/JpegEncoder/Source/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-gcc -s -fPIC -shared -fvisibility=hidden -O3 -ffast-math -mtune=corei7 \
-    JpegEncoder.c -o ../x64/libJpegEncoder.so
---
+++ Assets/Plugins/JpegEncoder/build.cmd
@@ -0,0 +1,9 @@
+@echo off
+
+git clone --depth=1 -b 2.0.0 https://github.com/libjpeg-turbo/libjpeg-turbo
+cd libjpeg-turbo
+cmake -G "Visual Studio 15 2017 Win64" -DCMAKE_BUILD_TYPE=Release -DENABLE_SHARED=ON -DENABLE_STATIC=OFF -DREQUIRE_SIMD=ON .
+cmake --build . --config Release
+move release\turbojpeg.dll ..\x64\
+cd ..
+rd /s /q libjpeg-turbo

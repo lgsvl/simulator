0ba067bb1e1646848a046c69f8e5422d2cef03e2
Martins Mozeiko <martins.mozeiko@lge.com>
2018-08-31 13:09:21 -0700

    Using libjpeg-turbo for faster jpeg encoding
    For 1080p images encoding performance is increased ~10x.

--- Assets/Plugins/JpegEncoder/JpegEncoder.cs
+++ Assets/Plugins/JpegEncoder/JpegEncoder.cs
@@ -6,6 +6,7 @@
  */
 
 
+using System;
 using System.Runtime.InteropServices;
 
 using Unity.Collections;
@@ -13,8 +14,55 @@ using Unity.Collections.LowLevel.Unsafe;
 
 public class JpegEncoder
 {
-    [DllImport("JpegEncoder")]
-    static extern unsafe int JpegEncoder_Encode(void* source, int width, int height, int components, int quality, void* output, int maxout);
+    enum Subsample : int
+    {
+        Samp444 = 0,
+        Samp422,
+        Samp420,
+        Gray,
+        Samp440,
+        Samp411,
+    }
+
+    enum PixelFormat : int
+    {
+        RGB = 0,
+        BGR,
+        RGBX,
+        BGRX,
+        XBGR,
+        XRGB,
+        GRAY,
+        RGBA,
+        BGRA,
+        ABGR,
+        ARGB,
+        CMYK,
+        Unknown = -1,
+    }
+
+    [System.Flags]
+    enum Flags : int
+    {
+        BottomUp = 2,
+        FastUpsample = 256,
+        NoRealloc = 1024,
+        FastDCT = 2048,
+        AccurateDCT = 4096,
+        StopOnWarning = 8192,
+        Progressive = 16384,
+    }
+
+    [DllImport("turbojpeg")]
+    static extern IntPtr tjInitCompress();
+
+    [DllImport("turbojpeg")]
+    static extern int tjDestroy(IntPtr handle);
+
+    [DllImport("turbojpeg")]
+    static extern int tjCompress2(IntPtr handle, IntPtr input,
+        int width, int pitch, int height, PixelFormat pixelFormat,
+        ref IntPtr output, ref ulong size, Subsample subsample, int quality, Flags flags);
 
     public static int Encode(NativeArray<byte> data, int width, int height, int components, int quality, byte[] result)
     {
@@ -23,7 +71,34 @@ public class JpegEncoder
             void* inPtr = NativeArrayUnsafeUtility.GetUnsafeReadOnlyPtr(data);
             fixed (byte* outPtr = result)
             {
-                return JpegEncoder_Encode(inPtr, width, height, components, quality, outPtr, result.Length);
+                IntPtr handle = tjInitCompress();
+                try
+                {
+                    IntPtr buffer = (IntPtr)outPtr;
+
+                    // careful with this, on Windows C long is 32-bit, but we use C# long which is always 64-bit
+                    // but as long as Windows is little-endian, it'll work fine
+                    ulong buffer_size = (ulong)result.Length; 
+
+                    int ok = tjCompress2(
+                        handle,
+                        (IntPtr)inPtr,
+                        width,
+                        width * components,
+                        height,
+                        components == 3 ? PixelFormat.RGB : PixelFormat.RGBX,
+                        ref buffer,
+                        ref buffer_size,
+                        Subsample.Samp420,
+                        quality,
+                        Flags.FastDCT | Flags.BottomUp | Flags.NoRealloc);
+
+                    return (ok == 0) ? (int)buffer_size : -1;
+                }
+                finally
+                {
+                    tjDestroy(handle);
+                }
             }
         }
     }
---
+++ Assets/Plugins/JpegEncoder/build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -eu
+
+git clone --depth=1 -b 2.0.0 https://github.com/libjpeg-turbo/libjpeg-turbo
+cd libjpeg-turbo
+cmake -DCMAKE_BUILD_TYPE=Release -DENABLE_SHARED=ON -DENABLE_STATIC=OFF -DREQUIRE_SIMD=ON .
+cmake --build . --config Release -- -j`nproc`
+strip --strip-unneeded libturbojpeg.so
+cp libturbojpeg.so ../x64/
+cd ..
+rm -rf libjpeg-turbo

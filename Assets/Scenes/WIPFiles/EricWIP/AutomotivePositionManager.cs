6af1a60bd17b3b38a06459e48d9f492dce263b76
eric.boise <eric.boise@lge.com>
2018-09-11 11:53:16 -0700

    init scene to work wip

---
+++ Assets/Scenes/WIPFiles/EricWIP/AutomotivePositionManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutomotivePositionManager : MonoBehaviour
+{
+	#region singleton
+
+	
+	#endregion
+}

18396b6a92098f993834c805a8efc161b13a1878
eric.boise <eric.boise@lge.com>
2018-09-18 12:57:39 -0700

    god ray scripts

---
+++ Assets/DemoOnly/DemoScripts/GodRayFaderComponent.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GodRayFaderComponent : MonoBehaviour
+{
+    public Color fadeColor;
+    private Color initColor;
+    private Color tempColor;
+    private Renderer thisRenderer;
+    private Material thisMaterial;
+    private float fadeTime;
+
+    private void Awake()
+    {
+        thisRenderer = GetComponent<Renderer>();
+        thisMaterial = thisRenderer.material;
+        initColor = thisMaterial.GetColor("_TintColor");
+    }
+
+    private void Start()
+    {
+        StartCoroutine(FadeMaterialAlpha());
+    }
+
+    private void Update()
+    {
+        //tempColor = Color.Lerp(initColor, fadeColor, Mathf.PingPong(Time.time * Random.Range(0.5f, 1f), 1.0f));
+        //thisRenderer.material.SetColor("_TintColor", tempColor);
+    }
+
+    IEnumerator FadeMaterialAlpha()
+    {
+        yield return new WaitForSeconds(Random.Range(0f, 25f));
+
+        float elapsedTime = 0f;
+        float randomTime = Random.Range(25f, 100f);
+        while (elapsedTime < randomTime)
+        {
+            thisRenderer.material.SetColor("_TintColor", Color.Lerp(initColor, fadeColor, (elapsedTime / randomTime)));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        //yield return new WaitForSeconds(Random.Range(5f, 25f));
+
+        elapsedTime = 0f;
+        randomTime = Random.Range(25f, 100f);
+        while (elapsedTime < randomTime)
+        {
+            thisRenderer.material.SetColor("_TintColor", Color.Lerp(fadeColor, initColor, (elapsedTime / randomTime)));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        StartCoroutine(FadeMaterialAlpha());
+    }
+}

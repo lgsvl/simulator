02203291bf11db44a464bac2a2b35326405afaf0
ruudy.liu <ruudy.liu@lge.com>
2018-09-21 17:45:01 -0700

    change to custom ui layer, add some editor tool scripts

--- Assets/Scripts/Editor/CommonToolSet.cs
+++ Assets/Scripts/Editor/CommonToolSet.cs
@@ -4,10 +4,23 @@ using System.Collections.Generic;
 
 public class CommonToolSet : UnityEditor.Editor
 {
-    [MenuItem("Tools/Number of objects selected")]
+    [MenuItem("Window/Custom Editor Tools/Number of objects selected")]
     static void OutputNumber()
     {
         GameObject[] selection = Selection.gameObjects;
         Debug.Log("Number of game objects selected: " + selection.Length);
+    }
+
+    [MenuItem("Window/Custom Editor Tools/Select All Renderers Under Selection")]
+    static void SelectAllRenderersUnderSelection()
+    {
+        GameObject[] selection = Selection.gameObjects;
+        List<Renderer> newRenderers = new List<Renderer>();
+        foreach (var go in selection)
+        {
+            newRenderers.AddRange(go.GetComponentsInChildren<Renderer>(true));
+        }
+
+        Selection.objects = newRenderers.ToArray();
     }
 }
---
+++ Assets/Scripts/RendererConstrainer.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[RequireComponent(typeof(Renderer))]
+public class RendererConstrainer : MonoBehaviour
+{
+    private List<Renderer> activeRenderers = new List<Renderer>();
+    bool isHidden = false;
+    private List<Renderer> hiddenRenderers = new List<Renderer>();
+
+    public void ReloadAllActiveRenderers()
+    {
+        ShowHiddenRenderers();
+        ClearAll();
+        foreach (var rend in FindObjectsOfType<Renderer>())
+        {
+            if (rend.enabled)
+            {
+                activeRenderers.Add(rend);
+            }
+        }
+    }
+
+    public void ToggleActiveRenderersOutsideBox()
+    {
+        if (isHidden)
+        {
+            ShowHiddenRenderers();
+            isHidden = false;
+        }
+        else
+        {
+            HideOutsideActiveRenderers();
+            isHidden = true;
+        }
+    }
+
+    public void ShowHiddenRenderers()
+    {
+        foreach (var rend in hiddenRenderers)
+        {
+            rend.enabled = true;
+        }
+        hiddenRenderers.Clear();
+    }
+
+    public void HideOutsideActiveRenderers()
+    {
+        hiddenRenderers.Clear();
+        var selfRend = GetComponent<Renderer>();
+        foreach (var rend in activeRenderers)
+        {
+            if (!selfRend.bounds.Intersects(rend.bounds))
+            {
+                rend.enabled = false;
+                hiddenRenderers.Add(rend);
+            }
+        }
+    }
+
+    public void ClearAll()
+    {
+        activeRenderers.Clear();
+        hiddenRenderers.Clear();
+        isHidden = false;
+    }
+
+    public void Test()
+    {
+        Debug.Log(activeRenderers.Count);
+    }
+}

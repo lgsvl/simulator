bd3534a5902beaa937049feccda93770138dc9d4
ruudy.liu <ruudy.liu@lge.com>
2018-10-04 12:28:04 -0700

    fix color segment camera and depth camera aspect ratio problem when switch camera type and view position

--- Assets/Scripts/DepthCameraEnabler.cs
+++ Assets/Scripts/DepthCameraEnabler.cs
@@ -17,12 +17,18 @@ public class DepthCameraEnabler : MonoBehaviour
 
             if (active)
             {
+                int w = TextureDisplay.renderCamera.targetTexture.width;
+                int h = TextureDisplay.renderCamera.targetTexture.height;
+                int d = TextureDisplay.renderCamera.targetTexture.depth;
+                var f = TextureDisplay.renderCamera.targetTexture.format;
                 TextureDisplay.renderCamera = PreviousCamera;
+                TextureDisplay.renderCamera.targetTexture = new RenderTexture(w, h, d, f);
             }
             else
             {
                 PreviousCamera = TextureDisplay.renderCamera;
                 TextureDisplay.renderCamera = Camera.GetComponent<Camera>();
+                TextureDisplay.renderCamera.targetTexture = new RenderTexture(PreviousCamera.targetTexture.width, PreviousCamera.targetTexture.height, PreviousCamera.targetTexture.depth, PreviousCamera.targetTexture.format);
             }
         }
     }

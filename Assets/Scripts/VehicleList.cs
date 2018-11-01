bc92285a17a84c2ede00c81c334227490036acb6
Martins Mozeiko <martins.mozeiko@lge.com>
2018-11-01 14:12:08 -0700

    Hides vehicle list and IP addresses on space key.

---
+++ Assets/Scripts/VehicleList.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class VehicleList : MonoBehaviour
+{
+    public RectTransform Panel;
+
+    void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // only toggle visibility if there is at least one vehicle in scene
+            if (Panel.transform.childCount > 2)
+            {
+                Panel.gameObject.SetActive(!Panel.gameObject.activeSelf);
+            }
+        }
+    }
+}

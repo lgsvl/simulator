f6875b54aa065924fa55c7ed19544c509c205f43
Martins Mozeiko <martins.mozeiko@lge.com>
2018-11-06 19:37:44 -0800

    Automatically get height where to respawn the car position.

--- Assets/Scripts/VehiclePositionResetter.cs
+++ Assets/Scripts/VehiclePositionResetter.cs
@@ -18,7 +18,18 @@ public class VehiclePositionResetter : MonoBehaviour, Ros.IRosClient
         Bridge.Subscribe<Ros.Vector3>(ResetTopic, msg =>
         {
             var position = GpsDevice.GetPosition(msg.x, msg.y);
-            position.y = 10.0f; // TODO: raycast to find ground?
+
+            int mask = ~(1 << 8); // car layer
+            RaycastHit hit;
+            if (Physics.Raycast(position + new Vector3(0, 100, 0), new Vector3(0, -1, 0), out hit, Mathf.Infinity, mask))
+            {
+                position = hit.point;
+                position.y += 0.01f;
+            }
+            else
+            {
+                position.y += 20.0f;
+            }
             var rotation = Quaternion.AngleAxis((float)msg.z * Mathf.Rad2Deg, Vector3.up);
             transform.SetPositionAndRotation(position, rotation);
         });

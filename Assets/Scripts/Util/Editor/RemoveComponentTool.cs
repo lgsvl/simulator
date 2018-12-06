ac7603d3adf3dd9019c150cc1e6ede0554e301f0
eric.boise <eric.boise@lge.com>
2018-12-05 18:32:38 -0800

    wip

---
+++ Assets/Scripts/Util/Editor/RemoveComponentTool.cs
@@ -0,0 +1,93 @@
+﻿/**
+ * Copyright (c) 2018 LG Electronics, Inc.
+ *
+ * This software contains code licensed as described in LICENSE.
+ *
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class RemoveComponentTool : ScriptableWizard
+{
+    public enum ComponentType
+    {
+        Collider,
+        BoxCollider,
+        MeshCollider,
+        CapsuleCollider,
+        SphereCollider,
+        WheelCollider,
+        Custom
+    };
+
+    [Tooltip("Select component type")]
+    public ComponentType componentType;
+
+    [Tooltip("Custom component type")]
+    public string customComponent;
+
+    [Tooltip("Remove child components")]
+    public bool isRemoveChildren = false;
+
+    [Tooltip("Replace with component?")]
+    public bool isReplace = false;
+
+    [Tooltip("Select replacement component type")]
+    public ComponentType replacementComponentType;
+
+    [Tooltip("Custom replacement component type")]
+    public string customReplacementComponent;
+
+    [MenuItem("SimulatorUtil/Remove Component Tool")]
+    static void CreateWizard()
+    {
+        DisplayWizard("Remove Component Tool", typeof(RemoveComponentTool), "Remove");
+    }
+
+    void OnWizardCreate()
+    {
+        
+        foreach (Transform t in Selection.transforms)
+        {
+            if (isRemoveChildren)
+            {
+                //Undo.DestroyObjectImmediate(t.GetComponentsInChildren(System.Type.GetType(componentType.ToString()), true)); // NEEDS OBJECT[]
+
+                //Undo.DestroyObjectImmediate(t.GetComponentsInChildren(System.Type.GetType(customComponent), true));
+            }
+            else
+            {
+                Undo.DestroyObjectImmediate(t.GetComponent(componentType.ToString()));
+
+                Undo.DestroyObjectImmediate(t.GetComponent(customComponent));
+            }
+
+            if (componentType != ComponentType.Custom)
+            {
+                Undo.DestroyObjectImmediate(t.GetComponent(componentType.ToString()));
+            }
+            else
+            {
+                Undo.DestroyObjectImmediate(t.GetComponent(customComponent));
+            }
+
+            if (isReplace)
+            {
+                //
+            }
+            else
+            {
+                //
+            }
+        }
+
+        //foreach (GameObject go in Selection.gameObjects)
+        //{
+        //    Undo.DestroyObjectImmediate(go);
+        //}
+    }
+
+}
---
+++ Assets/Scripts/Util/Editor/ReplacePrefabTool.cs
@@ -0,0 +1,59 @@
+﻿/**
+ * Copyright (c) 2018 LG Electronics, Inc.
+ *
+ * This software contains code licensed as described in LICENSE.
+ *
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ReplacePrefabTool : ScriptableWizard
+{
+    [Tooltip("Prefab that replaces selected objects")]
+    public GameObject replacePrefab;
+    [Tooltip("Parent object - optional")]
+    public GameObject customParent;
+
+    [Tooltip("Apply new rotation")]
+    public bool isCustomRot = false;
+    public Vector3 customRot = Vector3.zero;
+
+    [Tooltip("Apply new scale")]
+    public bool isCustomScale = false;
+    public Vector3 customScale = Vector3.zero;
+
+    [MenuItem("SimulatorUtil/Replace Prefab Tool")]
+    static void CreateWizard()
+    {
+        DisplayWizard("Replace Prefab Tool", typeof(ReplacePrefabTool), "Replace");
+    }
+
+    void OnWizardCreate()
+    {
+        foreach (Transform t in Selection.transforms)
+        {
+            GameObject newObject = PrefabUtility.InstantiatePrefab(replacePrefab) as GameObject;
+            Undo.RegisterCreatedObjectUndo(newObject, "created prefab");
+            Transform newT = newObject.transform;
+            newObject.name = replacePrefab.name;
+
+            // parent
+            if (customParent != null)
+                newT.SetParent(customParent.transform);
+            else
+                newT.SetParent(t.parent);
+
+            newT.position = t.position;
+            newT.localRotation = isCustomRot ? Quaternion.Euler(customRot) : t.localRotation;
+            newT.localScale = isCustomScale ? customScale : t.localScale;
+        }
+
+        foreach (GameObject go in Selection.gameObjects)
+        {
+            Undo.DestroyObjectImmediate(go);
+        }
+    }
+}

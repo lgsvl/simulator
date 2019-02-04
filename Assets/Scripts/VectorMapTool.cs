51527caf988ed6a2521d596a1ad497c43a625ef7
MasayaKataoka <masaya.kataoka@tier4.jp>
2019-02-04 15:17:14 +0900

    enables to fill BNID and FNID field.

--- Assets/Scripts/VectorMapTool.cs
+++ Assets/Scripts/VectorMapTool.cs
@@ -455,9 +455,12 @@ namespace Map
                             if (i > 0)
                             {
                                 beforeLaneID = Lanes.Count;
-                                var beforeLane = Lanes[beforeLaneID - 1];
-                                beforeLane.FLID = laneId;
-                                Lanes[beforeLaneID - 1] = beforeLane; //This is needed for struct copy to be applied back
+                                if (Lanes.Count != 0)
+                                {
+                                    var beforeLane = Lanes[beforeLaneID - 1];
+                                    beforeLane.FLID = laneId;
+                                    Lanes[beforeLaneID - 1] = beforeLane; //This is needed for struct copy to be applied back
+                                }
                                 var vmLane = Lane.MakeLane(laneId, vmDTLane.DID, beforeLaneID, 0, (nodes.Count-1), nodes.Count,laneInfos[i].laneCount, laneInfos[i].laneNumber);
                                 Lanes.Add(vmLane);
                                 if (i == 0)
@@ -482,7 +485,7 @@ namespace Map
                         if (lnSeg.befores.Count > 0)
                         {
                             var segStartId = terminalIdPair[(int)TerminalType.START];
-                            var ln = Lanes[segStartId - 1];
+                            var ln = Lanes[segStartId];
 
                             for (int i = 0; i < lnSeg.befores.Count; i++)
                             {
@@ -507,7 +510,7 @@ namespace Map
                                 }
                             }
 
-                            Lanes[segStartId - 1] = ln;
+                            Lanes[segStartId] = ln;
                         }
 
                         if (lnSeg.afters.Count > 0)
@@ -543,7 +546,7 @@ namespace Map
 
                             //Adjust last dtlane of each lane segment
                             var endDtLn = DtLanes[Lanes[segEndId - 1].DID - 1];
-                            var DtLnAfter = DtLanes[Lanes[afterSegStartLn - 1].DID - 1];
+                            var DtLnAfter = DtLanes[Lanes[afterSegStartLn].DID - 1];
 
                             var pointPos = VectorMapUtility.GetUnityPosition(new VectorMapPosition() { Bx = points[endDtLn.PID - 1].Bx, Ly = points[endDtLn.PID - 1].Ly, H = points[endDtLn.PID - 1].H }, exportScaleFactor);
                             var pointAfterPos = VectorMapUtility.GetUnityPosition(new VectorMapPosition() { Bx = points[DtLnAfter.PID - 1].Bx, Ly = points[DtLnAfter.PID - 1].Ly, H = points[DtLnAfter.PID - 1].H }, exportScaleFactor);

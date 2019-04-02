5e09a8fd921c54f8a16dd7df782101844f5d5451
Eric Sterner <eric.sterner@lge.com>
2019-04-01 18:29:36 -0700

    Initial commit.

---
+++ Assets/Editor/CreateAssetBundles.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+using System.IO;
+
+public class CreateAssetBundles
+{
+    [MenuItem("Assets/Build AssetBundles")]
+    static void BuildAllAssetBundles()
+    {
+        string assetBundleDirectory = "Assets/AssetBundles";
+        if (!Directory.Exists(assetBundleDirectory))
+        {
+            Directory.CreateDirectory(assetBundleDirectory);
+        }
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+    }
+}

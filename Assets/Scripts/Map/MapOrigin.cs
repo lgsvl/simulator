b62caa5f5c535456f145863a58f5406c1456a8cf
Qiang Lu <qiang1.lu@lge.com>
2019-04-04 12:45:33 -0700

    Set default values to 0 in MapOrigin, add it to 2 maps

--- Assets/Scripts/Map/MapOrigin.cs
+++ Assets/Scripts/Map/MapOrigin.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MapOrigin : MonoBehaviour {
-	public float OriginNorthing = 4182486.0f;
-	public float OriginEasting = 552874.0f;
-	public float Angle = -45.3f;
-	public int UTMZoneId = 10;
+	public float OriginNorthing = .0f;
+	public float OriginEasting = .0f;
+	public float Angle = .0f;
+	public int UTMZoneId = 0;
 	// Use this for initialization
 	void Start () {
 		

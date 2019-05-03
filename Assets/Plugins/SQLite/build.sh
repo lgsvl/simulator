2b680275cd421a62621907946cfb9ccb519c42af
Martins Mozeiko <martins.mozeiko@lge.com>
2019-05-02 18:51:14 -0700

    Add native SQLite library for Linux

---
+++ Assets/Plugins/SQLite/build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -eu
+
+wget -O - https://www.sqlite.org/2019/sqlite-autoconf-3280000.tar.gz | tar xfz -
+cd sqlite-autoconf-*
+./configure
+make libsqlite3.la
+cp .libs/libsqlite3.so ../x64/
+strip --strip-unneeded ../x64/libsqlite3.so
+cd ..
+rm -rf sqlite-autoconf-*

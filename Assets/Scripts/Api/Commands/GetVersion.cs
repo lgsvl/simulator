db435d6740c3b0b686db8ac6451abac7c5d3316e
Dmitry Zelenkovsky <Dmitry.Zelenkovsky@lge.com>
2019-06-17 18:46:03 -0700

    Update license headers to avoid warnings.

--- Assets/Scripts/Api/Commands/GetVersion.cs
+++ Assets/Scripts/Api/Commands/GetVersion.cs
@@ -1,9 +1,9 @@
 /**
-* Copyright (c) 2019 LG Electronics, Inc.
-*
-* This software contains code licensed as described in LICENSE.
-*
-*/
+ * Copyright (c) 2019 LG Electronics, Inc.
+ *
+ * This software contains code licensed as described in LICENSE.
+ *
+ */
 
 using SimpleJSON;
 using Simulator.Utilities;

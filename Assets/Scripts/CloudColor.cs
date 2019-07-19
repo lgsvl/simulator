b63b01d03507efa89381733784ddb1323600d07f
eric.sterner <eric.sterner@lge.com>
2019-07-19 01:30:11 +0000

    adding the new clouds prefab

---
+++ Assets/Scripts/CloudColor.cs
@@ -0,0 +1,25 @@
+﻿/**
+ * Copyright (c) 2019 LG Electronics, Inc.
+ *
+ * This software contains code licensed as described in LICENSE.
+ *
+ */
+
+using System.Collections;
+using UnityEngine;
+
+public class CloudColor : MonoBehaviour
+{ 
+    private Light sun;
+    public Material cloudMaterial;
+
+    private void Start()
+    {
+        sun = SimulatorManager.Instance.EnvironmentEffectsManager.sunGO.GetComponent<Light>();
+    }
+
+    void Update()
+    {
+        cloudMaterial.SetColor("_SunCloudsColor", sun.color);
+    }
+}

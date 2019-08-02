22edbe7742dab6c3ed8bb4bd1954fe9c9f1c03bf
Martins Mozeiko <martins.mozeiko@lge.com>
2019-08-01 17:25:29 -0700

    Draw 2d bounding boxes without geometry shader.
    This fixes Unity bug with shader compilation under Vulkan.

--- Assets/Scripts/Utilities/AAWireBox.cs
+++ Assets/Scripts/Utilities/AAWireBox.cs
@@ -8,6 +8,8 @@
 using UnityEngine;
 using System.Collections.Generic;
 using Unity.Collections.LowLevel.Unsafe;
+using System;
+using UnityEngine.Rendering;
 
 namespace Simulator.Utilities
 {
@@ -20,9 +22,15 @@ namespace Simulator.Utilities
             public Vector3 Color;
         }
 
+        struct Vertex
+        {
+            public Vector3 Position;
+            public Vector3 Color;
+        }
+
         const int BufferGranularity = 1024;
 
-        List<Box> Boxes;
+        List<Vertex> Vertices;
         ComputeBuffer Buffer;
 
         Material Material;
@@ -31,7 +39,7 @@ namespace Simulator.Utilities
 
         void Start()
         {
-            Boxes = new List<Box>(BufferGranularity);
+            Vertices = new List<Vertex>(BufferGranularity);
             Material = new Material(RuntimeSettings.Instance.AAWireBoxShader);
         }
 
@@ -43,39 +51,66 @@ namespace Simulator.Utilities
 
         public void Draw(Vector2 min, Vector2 max, Color color)
         {
-            Boxes.Add(new Box()
+            min.y *= -1;
+            max.y *= -1;
+
+            var size = new Vector2(LineWidth / Camera.pixelWidth, LineWidth / Camera.pixelHeight);
+
+            var p0 = new Vector2(min.x, max.y);
+            var p1 = new Vector2(max.x, min.y);
+
+            Action<Vector2, Vector2> line = (Vector2 a, Vector2 b) =>
             {
-                Min = min,
-                Max = max,
-                Color = (Vector4)color,
-            });
+                var dir = (b - a).normalized;
+                var normal = new Vector2(-dir.y, dir.x);
+
+                dir *= size;
+                normal *= size;
+
+                var v0 = a - dir + normal;
+                var v1 = a - dir - normal;
+                var v2 = b + dir + normal;
+                var v3 = b + dir - normal;
+
+                Vertices.Add(new Vertex() { Position = v0, Color = (Vector4)color });
+                Vertices.Add(new Vertex() { Position = v1, Color = (Vector4)color });
+                Vertices.Add(new Vertex() { Position = v2, Color = (Vector4)color });
+
+                Vertices.Add(new Vertex() { Position = v0, Color = (Vector4)color });
+                Vertices.Add(new Vertex() { Position = v2, Color = (Vector4)color });
+                Vertices.Add(new Vertex() { Position = v3, Color = (Vector4)color });
+            };
+
+            line(min, p0);
+            line(p0, max);
+            line(max, p1);
+            line(p1, min);
         }
 
         void LateUpdate()
         {
-            if (Boxes.Count == 0)
+            if (Vertices.Count == 0)
             {
                 return;
             }
 
-            if (Buffer == null || Boxes.Count > Buffer.count)
+            if (Buffer == null || Vertices.Count > Buffer.count)
             {
                 Buffer?.Release();
 
-                int newCount = ((Boxes.Count + BufferGranularity - 1) / BufferGranularity) * BufferGranularity;
-                Buffer = new ComputeBuffer(newCount, UnsafeUtility.SizeOf<Box>());
-                Material.SetBuffer("_Boxes", Buffer);
+                int newCount = ((Vertices.Count + BufferGranularity - 1) / BufferGranularity) * BufferGranularity;
+                Buffer = new ComputeBuffer(newCount, UnsafeUtility.SizeOf<Vertex>());
+                Material.SetBuffer("_Vertices", Buffer);
             }
 
-            Buffer.SetData(Boxes, 0, 0, Boxes.Count);
-            Material.SetFloat("_LineWidth", LineWidth);
+            Buffer.SetData(Vertices, 0, 0, Vertices.Count);
 
             // TODO: big number for size to include everything in scene
             var bounds = new Bounds(Vector3.zero, new Vector3(10000, 10000, 10000));
-            
-            Graphics.DrawProcedural(Material, bounds, MeshTopology.Points, Boxes.Count, 1, Camera, null, UnityEngine.Rendering.ShadowCastingMode.Off, true, layer: gameObject.layer);
 
-            Boxes.Clear();
+            Graphics.DrawProcedural(Material, bounds, MeshTopology.Triangles, Vertices.Count, 1, Camera, null, ShadowCastingMode.Off, false, layer: gameObject.layer);
+
+            Vertices.Clear();
         }
     }
 }
--- Assets/Shaders/AAWireBox.shader
+++ Assets/Shaders/AAWireBox.shader
@@ -19,98 +19,35 @@ Shader "Simulator/AAWireBox"
 
             CGPROGRAM
             #pragma vertex Vert
-            #pragma geometry Geom
             #pragma fragment Frag
 
             #include "UnityCG.cginc"
 
-            struct Box
+            struct Vertex
             {
-                float2 min;
-                float2 max;
-                float3 color;
+                float3 position;
+                nointerpolation float3 color;
             };
 
-            StructuredBuffer<Box> _Boxes;
+            StructuredBuffer<Vertex> _Vertices;
 
-            float _LineWidth;
-
-            struct v2g
-            {
-                float2 min : MIN;
-                float2 max : MAX;
-                float3 color : COLOR;
-            };
-
-            struct g2f
+            struct v2f
             {
                 float4 position : SV_POSITION;
                 float3 color : COLOR;
             };
 
-            v2g Vert(uint id : SV_VertexID)
+            v2f Vert(uint id : SV_VertexID)
             {
-                Box box = _Boxes[id];
+                Vertex v = _Vertices[id];
 
-                v2g output;
-                output.min = float4(box.min, 0, 1);
-                output.max = float4(box.max, 0, 1);
-                output.color = box.color;
+                v2f output;
+                output.position = float4(v.position, 1);
+                output.color = v.color;
                 return output;
             }
 
-            void Vertex(float4 vertex, float2 normal, float3 color, inout TriangleStream<g2f> output)
-            {
-                g2f o;
-
-                o.position = vertex + float4(normal, 0, 0);
-                o.color = color;
-                output.Append(o);
-
-                o.position = vertex - float4(normal, 0, 0);
-                o.color = color;
-                output.Append(o);
-            }
-
-            void Line(float4 v0, float4 v1, float3 color, inout TriangleStream<g2f> output)
-            {
-                float2 dir = normalize((v1.xy / v1.w - v0.xy / v0.w).xy);
-                float2 normal = float2(-dir.y, dir.x);
-
-                float2 size = _LineWidth / _ScreenParams.xy;
-
-                dir *= size;
-                normal *= size;
-
-                Vertex(v0 - float4(dir * v0.w, 0, 0), normal * v0.w, color, output);
-                Vertex(v1 + float4(dir * v1.w, 0, 0), normal * v1.w, color, output);
-
-                output.RestartStrip();
-            }
-
-            [maxvertexcount(16)]
-            void Geom(point v2g input[1], inout TriangleStream<g2f> output)
-            {
-                float3 color = input[0].color;
-                
-                float4 min = float4(input[0].min, 0, 1);
-                min.y *= -1;
-                float4 max = float4(input[0].max, 0, 1);
-                max.y *= -1;
-                float4 p0 = float4(min.x, max.y, 0, 1);
-                float4 p1 =  float4(max.x, min.y, 0, 1);
-
-#ifndef SHADER_API_VULKAN
-                // TODO: on Vulkan this produces very strange error:
-                // syntax error, unexpected LEFT_BRACKET at line 25 (on vulkan)
-                Line(min, p0, color, output);
-                Line(p0, max, color, output);
-                Line(max, p1, color, output);
-                Line(p1, min, color, output);
-#endif
-            }
-
-            float4 Frag(g2f input) : SV_Target
+            float4 Frag(v2f input) : SV_Target
             {
                 return float4(input.color, 1.0f);
             }

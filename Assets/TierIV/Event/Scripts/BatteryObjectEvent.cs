2b120a3c0fa004eb698c0df0f1270b6b67288888
kaichi.oda <kaichi.oda@tier4.jp>
2019-10-10 15:31:45 +0900

    event notify sample impementation

---
+++ Assets/TierIV/Event/Scripts/BatteryObjectEvent.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TierIV.Event;
+
+
+public class BatteryObjectEvent : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    void OnEnable()
+    {
+        EventNotifier.Instance.SubscribeEvent(OnReceiveChargeEvent, "Charge");
+    }
+
+    void OnDisable()
+    {
+        EventNotifier.Instance.UnSubscribeEvent(OnReceiveChargeEvent);
+    }
+
+    void OnReceiveChargeEvent(string chargeEventJson)
+    {
+        // parse chargeEventJson to get an appropriate value ...
+    }
+}
---
+++ Assets/TierIV/Event/Scripts/ChargeStationEvent.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TierIV.Event;
+
+public class ChargeStationEvent : MonoBehaviour
+{
+    struct BatteryVolume
+    {
+        public float parcentage;
+    }
+
+    BatteryVolume volume = new BatteryVolume { parcentage = 10f };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerStay(Collider target)
+    {
+        // targetに通知
+        EventNotifier.Instance.BroadcastEvent("Charge", JsonUtility.ToJson(volume));
+    }
+
+}

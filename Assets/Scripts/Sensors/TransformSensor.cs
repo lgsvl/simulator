3cbc8116dcb58fe0b9f0d6ce09ff580b414157c6
mark.gerow <mark.gerow@lge.com>
2020-01-24 11:48:24 -0800

    Transform sensor

---
+++ Assets/Scripts/Sensors/TransformSensor.cs
@@ -0,0 +1,29 @@
+/**
+ * Copyright (c) 2020 LG Electronics, Inc.
+ *
+ * This software contains code licensed as described in LICENSE.
+ *
+ */
+
+using Simulator.Bridge;
+using Simulator.Sensors.UI;
+using Simulator.Utilities;
+
+namespace Simulator.Sensors
+{
+    [SensorType("Transform", new System.Type[] { })]
+    public class TransformSensor : SensorBase
+    {
+        public override void OnBridgeSetup(IBridge bridge)
+        {
+        }
+
+        public override void OnVisualize(Visualizer visualizer)
+        {
+        }
+
+        public override void OnVisualizeToggle(bool state)
+        {
+        }
+    }
+}

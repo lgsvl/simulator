9ac37e9460b22e89e77d9f446cacdf9051126c04
Guodong Rong <guodong.rong@lge.com>
2020-03-19 16:21:14 -0700

    Refactor Lidar and VelodyneLidar

---
+++ Assets/Scripts/Utilities/ByteArray.cs
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2019 LG Electronics, Inc.
+ *
+ * This software contains code licensed as described in LICENSE.
+ *
+ */
+
+using Unity.Collections;
+
+namespace Simulator.Utilities
+{
+    public static class ByteArrayUtils 
+    {
+        // Helper function to fill in an unsinged 32-bit integer at specific position in a byte array.
+        public static void FillInFourBytes(uint value, byte[] data, int byteIndex)
+        {
+            // The simulator can only run on little endian platforms.
+            // So we assume the endianness is always little endian.
+            data[byteIndex] = (byte)(value & 0xFF);
+            value >>= 8;
+            data[byteIndex + 1] = (byte)(value & 0xFF);
+        }
+        
+        // Helper function to fill in an unsigned 16-bit integer at specific position in a native byte array.
+        public static void FillInTowBytes(ushort value, NativeArray<byte> data, int byteIndex)
+        {
+            // The simulator can only run on little endian platforms.
+            // So we assume the endianness is always little endian.
+            data[byteIndex] = (byte)(value & 0xFF);
+            value >>= 8;
+            data[byteIndex + 1] = (byte)(value & 0xFF);
+        }
+
+        // Helper function to fill in an unsinged 32-bit integer at specific position in a native byte array.
+        public static void FillInFourBytes(uint value, NativeArray<byte> data, int byteIndex)
+        {
+            // The simulator can only run on little endian platforms.
+            // So we assume the endianness is always little endian.
+            data[byteIndex] = (byte)(value & 0xFF);
+            value >>= 8;
+            data[byteIndex + 1] = (byte)(value & 0xFF);
+            value >>= 8;
+            data[byteIndex + 2] = (byte)(value & 0xFF);
+            value >>= 8;
+            data[byteIndex + 3] = (byte)(value & 0xFF);
+        }
+    }
+}

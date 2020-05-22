fcd5f4afe0eea0505f58443780f56014074f179d
Guodong Rong <guodong.rong@lge.com>
2020-05-22 10:43:30 -0700

    Add set_speed for Pedestrian.

--- Assets/Scripts/Controllers/PedestrianController.cs
+++ Assets/Scripts/Controllers/PedestrianController.cs
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2019 LG Electronics, Inc.
+ * Copyright (c) 2019-2020 LG Electronics, Inc.
  *
  * This software contains code licensed as described in LICENSE.
  *
@@ -173,6 +173,11 @@ public class PedestrianController : DistributedComponent, IGloballyUniquelyIdent
 
         ThisPedState = PedestrianState.None;
         Control = ControlType.Manual;
+    }
+
+    public void SetSpeed(float speed)
+    {
+        LinearSpeed = speed;
     }
     #endregion
 

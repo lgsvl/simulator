338840697386c37544cec8d4d2c4f2eedf589ec9
Karol Byczkowski <karol.byczkowski@lge.com>
2020-08-19 19:22:02 +0200

    VSE - increased LOD bias.

--- Assets/Scripts/ScenarioEditor/Managers/QualityChanger.cs
+++ Assets/Scripts/ScenarioEditor/Managers/QualityChanger.cs
@@ -7,6 +7,9 @@
 
 namespace Simulator.ScenarioEditor.Managers
 {
+    using System.Collections.Generic;
+    using System.Reflection;
+    using UnityEditor.SceneManagement;
     using UnityEngine;
     using UnityEngine.Rendering;
     using UnityEngine.Rendering.HighDefinition;
@@ -25,6 +28,18 @@ namespace Simulator.ScenarioEditor.Managers
         /// </summary>
         [SerializeField]
         private Volume volume;
+        
+        /// <summary>
+        /// Default settings of the HDRP
+        /// </summary>
+        [SerializeField]
+        private HDRenderPipelineAsset defaultHdrpSettings;
+        
+        /// <summary>
+        /// VSE settings of the HDRP
+        /// </summary>
+        [SerializeField]
+        private HDRenderPipelineAsset vseHdrpSettings;
 #pragma warning restore 0649
 
         private bool initialized;
@@ -54,6 +69,9 @@ namespace Simulator.ScenarioEditor.Managers
                 volume.profile.components.Find(component => component is HDShadowSettings) as HDShadowSettings;
             if (shadows != null)
                 shadows.maxShadowDistance.value *= QualityDistanceMultiplier;
+
+            QualitySettings.renderPipeline = vseHdrpSettings;
+            ReinitializeRenderPipeline();
             
             initialized = true;
         }
@@ -69,7 +87,65 @@ namespace Simulator.ScenarioEditor.Managers
             if (shadows != null)
                 shadows.maxShadowDistance.value /= QualityDistanceMultiplier;
             
+            QualitySettings.renderPipeline = defaultHdrpSettings;
+            ReinitializeRenderPipeline();
+            
             initialized = false;
+        }
+        
+        private static void ReinitializeRenderPipeline()
+        {
+            // NOTE: This is a workaround for Vulkan. Even if HDRP is reinitialized, lighting data and depth buffers
+            //       on render targets (even ones created afterwards) will be corrupted. Reloading scene before
+            //       forcefully reinitializing HDRP will refresh both lighting and depth data appropriately.
+            //       This happens automatically for scene bundles, but is required for prefab ones.
+            //       If this is not called for scene bundles, however, command line execution from async method will
+            //       not create render pipeline at all when using Vulkan and crash with invalid memory access
+            // Last tested on Unity 2019.3.15f1 and HDRP 7.3.1
+
+            var assetField = typeof(RenderPipelineManager).GetField("s_CurrentPipelineAsset", BindingFlags.NonPublic | BindingFlags.Static);
+            if (assetField == null)
+            {
+                Debug.LogError($"No asset field in {nameof(RenderPipelineManager)}. Did you update HDRP?");
+                return;
+            }
+
+            var asset = assetField.GetValue(null);
+            var cleanupMethod = typeof(RenderPipelineManager).GetMethod("CleanupRenderPipeline", BindingFlags.NonPublic | BindingFlags.Static);
+            if (cleanupMethod == null)
+            {
+                Debug.LogError($"No cleanup method in {nameof(RenderPipelineManager)}. Did you update HDRP?");
+                return;
+            }
+
+            cleanupMethod.Invoke(null, null);
+
+            var prepareMethod = typeof(RenderPipelineManager).GetMethod("PrepareRenderPipeline", BindingFlags.NonPublic | BindingFlags.Static);
+            if (prepareMethod == null)
+            {
+                Debug.LogError($"No prepare method in {nameof(RenderPipelineManager)}. Did you update HDRP?");
+                return;
+            }
+
+            prepareMethod.Invoke(null, new[] {asset});
+
+            var hdrp = RenderPipelineManager.currentPipeline as HDRenderPipeline;
+            if (hdrp == null)
+            {
+                var pipelineType = RenderPipelineManager.currentPipeline == null ? "null" : $"{RenderPipelineManager.currentPipeline.GetType().Name}";
+                Debug.LogError($"HDRP not available for preview. (type: {pipelineType}))");
+                return;
+            }
+
+            var pipelineReadyField = typeof(HDRenderPipeline).GetField("m_ResourcesInitialized", BindingFlags.NonPublic | BindingFlags.Instance);
+            if (pipelineReadyField == null)
+            {
+                Debug.LogError($"No ready flag in {nameof(HDRenderPipeline)}. Did you update HDRP?");
+                return;
+            }
+
+            if (!(bool) pipelineReadyField.GetValue(hdrp))
+                Debug.LogError("Failed to reinitialize HDRP");
         }
     }
 }

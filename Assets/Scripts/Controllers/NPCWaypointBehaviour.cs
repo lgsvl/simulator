58d6ace5208527fcf228e2fe8be4f546ff5f4e1a
eric.boise <eric.boise@lge.com>
2021-06-04 10:08:53 -0700

    add steer angle to waypoint follow

--- Assets/Scripts/Controllers/NPCWaypointBehaviour.cs
+++ Assets/Scripts/Controllers/NPCWaypointBehaviour.cs
@@ -170,6 +170,7 @@ public class NPCWaypointBehaviour : NPCBehaviourBase
             CurrentTarget = LaneData[CurrentIndex];
             controller.MovementSpeed = LaneSpeed[CurrentIndex];
             controller.currentSpeed = LaneSpeed[CurrentIndex];
+            controller.steerVector = (CurrentTarget - controller.frontCenter.position).normalized;
         }
 
         if (CurrentIndex == LaneData.Count)

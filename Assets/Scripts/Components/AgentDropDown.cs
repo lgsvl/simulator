3777e818510a0144586baad0da6f08182bf81921
eric.boise <eric.boise@lge.com>
2021-06-08 13:55:41 -0700

    Fix issue with agent dropdown not refreshing on click

--- Assets/Scripts/Components/AgentDropDown.cs
+++ Assets/Scripts/Components/AgentDropDown.cs
@@ -1,14 +1,20 @@
 /**
- * Copyright (c) 2019 LG Electronics, Inc.
+ * Copyright (c) 2019-2021 LG Electronics, Inc.
  *
  * This software contains code licensed as described in LICENSE.
  *
  */
 
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class AgentDropDown : MonoBehaviour
+public class AgentDropDown : MonoBehaviour ,IPointerClickHandler
 {
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        SimulatorManager.Instance.UIManager.SetAgentsDropdown();
+    }
+
     private void OnEnable()
     {
         SimulatorManager.Instance.UIManager.SetAgentsDropdown();

50d50f539d7a4bfacf59af02deb5afc8a2e9d7d9
Martin Jansa <martin.jansa@lge.com>
2021-08-27 16:43:32 +0200

    Jenkins: Checkout bundle sources to .external-assets
    * to prevent Unity importing all the available assets when we want to build just one of them
    * then inside the for loop in build-bundles.sh always move the source directory from
      .external-assets to External and move it back after the build
    * also check if the Asset bundle was created in expected location
    * this also helps when there are many Assets selected in Jenkins
      job parameters, but get-assets.sh filters only very few of them
      to be built (without FORCE_REBUILD being used)

--- Jenkins/build-bundles.sh
+++ Jenkins/build-bundles.sh
@@ -78,7 +78,17 @@ function get_unity_license {
 }
 
 function build_bundle {
+    # Move the source for individual bundles from Assets/External-All to Assets/External
+    # to prevent Unity importing all the available assets when we went to build just one
+    # of them
     echo "Building bundle $*"
+    if [ ! -d .external-assets/$1/$2 ] ; then
+        echo "ERROR: Bundle source doesn't exist in .external-assets/$1/$2"
+        exit 1
+    else
+        mkdir -p Assets/External/$1
+        mv .external-assets/$1/$2 Assets/External/$1
+    fi
 
     ${UNITY} \
         -force-vulkan \
@@ -86,8 +96,14 @@ function build_bundle {
         -projectPath /mnt \
         -executeMethod Simulator.Editor.Build.Run \
         -buildBundles \
-        $* \
+        -build$1 $2 \
         -logFile /dev/stdout
+
+    mv Assets/External/$1/$2 .external-assets/$1/$2
+    if [ ! -f AssetBundles/$1/*_$2 ] ; then
+        echo "ERROR: Bundle wasn't created in AssetBundles/$1/*_$2"
+        exit 1
+    fi
 }
 
 function finish
@@ -120,27 +136,27 @@ rm -Rf /mnt/AssetBundles || true
 if [ ! -z ${SIM_ENVIRONMENTS+x} ]; then
     getAssets "${SIM_ENVIRONMENTS}"
     for A in ${ASSETS}; do
-        build_bundle -buildEnvironments ${A} | tee -a unity-build-bundles.log
+        build_bundle Environments ${A} | tee -a unity-build-bundles.log
     done
 fi
 if [ ! -z ${SIM_VEHICLES+x} ]; then
     getAssets "${SIM_VEHICLES}"
     for A in ${ASSETS}; do
-        build_bundle -buildVehicles ${A} | tee -a unity-build-bundles.log
+        build_bundle Vehicles ${A} | tee -a unity-build-bundles.log
     done
 fi
 
 if [ ! -z ${SIM_SENSORS+x} ]; then
     getAssets "${SIM_SENSORS}"
     for A in ${ASSETS}; do
-        build_bundle -buildSensors ${A} | tee -a unity-build-bundles.log
+        build_bundle Sensors ${A} | tee -a unity-build-bundles.log
     done
 fi
 
 if [ ! -z ${SIM_BRIDGES+x} ]; then
     getAssets "${SIM_BRIDGES}"
     for A in ${ASSETS}; do
-        build_bundle -buildBridges ${A} | tee -a unity-build-bundles.log
+        build_bundle Bridges ${A} | tee -a unity-build-bundles.log
     done
 fi
 

0a65f082339dd46c36bcb3c3f126bb242a75e03b
Martin Jansa <martin.jansa@lge.com>
2021-09-16 20:04:54 +0200

    Jenkins: build-bundles: retry building each bundle upto 3 times
    * when rebuilding CubeTown many times in the same workspace I always get segfault like:
    
      19:06:26  Launched and connected shader compiler UnityShaderCompiler after 0.03 seconds
      19:06:44  Caught fatal signal - signo:11 code:1 errno:0 addr:0xd0000000e
      19:06:44  Obtained 25 stack frames.
      19:06:44  #0  0x007f72a328a3c0 in funlockfile
      19:06:44  #1  0x007f72a3141c5b in memcpy
      19:06:44  #2  0x007f71edcabf64 in vk_optimusGetDeviceProcAddr
      19:06:44  #3  0x0056107c6dad75 in vk::TaskExecutor::HandleCommandStream(GrowableBuffer*, VkCommandBuffer_T*)
      19:06:44  #4  0x0056107c6d9fe6 in vk::TaskExecutor::ExecuteCommandbuffers(vk::CommandBuffer**, int)
      19:06:44  #5  0x0056107c6d6c33 in vk::TaskExecutor::DoFlush(VkFence_T*, VkSemaphore_T*, unsigned long)
      19:06:44  #6  0x0056107c6a3e07 in GfxDeviceVK::SubmitCurrentCommandBuffers(VkSemaphore_T*)
      19:06:44  #7  0x0056107c6a5721 in GfxDeviceVK::FinishRendering()
      19:06:44  #8  0x0056107c61f4dd in GfxDeviceClient::FinishRendering()
      19:06:44  #9  0x0056107bf9b506 in AssetHotreload()
      19:06:44  #10 0x0056107f0fb8ff in AssetHotreload(core::hash_set<UnityGUID, core::hash<UnityGUID>, std::equal_to<UnityGUID> > const&)
      19:06:44  #11 0x0056107f1964e7 in RefreshInternalV2(AssetDatabase::UpdateAssetOptions, ScanFilter const&, InternalRefreshFlagsV2)
      19:06:44  #12 0x0056107f180712 in StopAssetImportingV2(AssetDatabase::UpdateAssetOptions, InternalRefreshFlagsV2, ScanFilter const*)
      19:06:44  #13 0x0056107f180cb1 in RefreshV2(AssetDatabase::UpdateAssetOptions)
      19:06:44  #14 0x0056107f0e5181 in AssetDatabase::Refresh(AssetDatabase::UpdateAssetOptions)
      19:06:44  #15 0x0056107e4fdeb3 in Application::InitializeProject()
      19:06:44  #16 0x0056107f03e606 in InitializeUnity(void*)
      19:06:44  #17 0x007f72a39c3be8 in g_source_get_time
      19:06:44  #18 0x007f72a39c304e in g_main_context_dispatch
      19:06:44  #19 0x007f72a39c3400 in g_main_context_dispatch
      19:06:44  #20 0x007f72a39c36f3 in g_main_loop_run
      19:06:44  #21 0x007f72a405837d in gtk_main
      19:06:44  #22 0x0056107f03dfce in main
      19:06:44  #23 0x007f72a30aa0b3 in __libc_start_main
      19:06:44  #24 0x0056107bd72029 in _start
      19:06:44  /usr/bin/unity-editor: line 11:    11 Segmentation fault      (core dumped) /opt/unity/Editor/Unity "$@"
    
      when building in workspace with empty "Library" directory and re-running
      it in the same workspace 2nd time (keeping "Library") finishes OK
    
      Rerun each bundle build upto 3 times before giving up and showing an error.
    
      Show summary at the end to easily find if some bundles still need to be
      re-triggered or fixed.
    
    * re-try the initial "fetch license" as well as it was sometimes failing
      there already (before starting any bundle build)
    
    * we do something similar in build-simulator.sh already, for "check" function

--- Jenkins/build-bundles.sh
+++ Jenkins/build-bundles.sh
@@ -67,21 +67,30 @@ fi
 function get_unity_license {
     echo "Fetching unity license"
 
-    ${UNITY} \
-        -logFile /dev/stdout \
-        -batchmode \
-        -serial ${UNITY_SERIAL} \
-        -username ${UNITY_USERNAME} \
-        -password ${UNITY_PASSWORD} \
-        -projectPath /mnt \
-        -quit
+    for i in `seq 1 3`; do
+        if ! ${UNITY} \
+            -batchmode \
+            -serial ${UNITY_SERIAL} \
+            -username ${UNITY_USERNAME} \
+            -password ${UNITY_PASSWORD} \
+            -silent-crashes \
+            -projectPath /mnt \
+            -logFile /dev/stdout \
+            -quit; then
+            echo "WARN: Fetching unity license failed, attempt ${i} in 3, trying again"
+        else
+            echo "INFO: Fetching unity license attempt ${i} seems to be successful"
+            break
+        fi
+    done
 }
 
 function build_bundle {
     # Move the source for individual bundles from Assets/External-All to Assets/External
     # to prevent Unity importing all the available assets when we went to build just one
     # of them
-    echo "Building bundle $*"
+    BUNDLE=$(expr ${BUNDLE} + 1)
+    echo "Building bundle (${BUNDLE}/${BUNDLES}) $*"
     if [ ! -d .external-assets/$1/$2 ] ; then
         echo "ERROR: Bundle source doesn't exist in .external-assets/$1/$2"
         exit 1
@@ -90,19 +99,29 @@ function build_bundle {
         mv .external-assets/$1/$2 Assets/External/$1
     fi
 
-    ${UNITY} \
-        -force-vulkan \
-        -silent-crashes \
-        -projectPath /mnt \
-        -executeMethod Simulator.Editor.Build.Run \
-        -buildBundles \
-        -build$1 $2 \
-        -logFile /dev/stdout
+    for i in `seq 1 3`; do
+        if ! ${UNITY} \
+            -force-vulkan \
+            -silent-crashes \
+            -projectPath /mnt \
+            -executeMethod Simulator.Editor.Build.Run \
+            -buildBundles \
+            -build$1 $2 \
+            -logFile /dev/stdout; then
+            echo "WARN: Bundle $1 $2 failed to build, attempt ${i} in 3, trying again"
+        else
+            echo "INFO: Bundle $1 $2 bundle attempt ${i} seems to be successful"
+            break
+        fi
+    done
+
 
     mv Assets/External/$1/$2 .external-assets/$1/$2
     if [ ! -f AssetBundles/$1/*_$2 ] ; then
-        echo "ERROR: Bundle wasn't created in AssetBundles/$1/*_$2"
+        echo "ERROR: Bundle $1 $2 wasn't created in AssetBundles/$1/*_$2"
         exit 1
+    else
+        echo "INFO: Bundle $1 $2 succeeded to build"
     fi
 }
 
@@ -131,33 +150,76 @@ fi
 
 echo "I: Cleanup AssetBundles before build"
 
-rm -Rf /mnt/AssetBundles || true
+rm -Rf /mnt/AssetBundles unity-build-bundles*log || true
+
+BUNDLES=0
+if [ ! -z ${SIM_ENVIRONMENTS+x} ]; then
+    echo "INFO: going to build $(echo "${SIM_ENVIRONMENTS=}" | wc -l) Environments"
+    BUNDLES=$(expr ${BUNDLES} + $(echo "${SIM_ENVIRONMENTS=}" | wc -l))
+fi
+if [ ! -z ${SIM_VEHICLES+x} ]; then
+    echo "INFO: going to build $(echo "${SIM_VEHICLES=}" | wc -l) Vehicles"
+    BUNDLES=$(expr ${BUNDLES} + $(echo "${SIM_VEHICLES=}" | wc -l))
+fi
+if [ ! -z ${SIM_SENSORS+x} ]; then
+    echo "INFO: going to build $(echo "${SIM_SENSORS=}" | wc -l) Sensors"
+    BUNDLES=$(expr ${BUNDLES} + $(echo "${SIM_SENSORS=}" | wc -l))
+fi
+if [ ! -z ${SIM_BRIDGES+x} ]; then
+    echo "INFO: going to build $(echo "${SIM_BRIDGES=}" | wc -l) Bridges"
+    BUNDLES=$(expr ${BUNDLES} + $(echo "${SIM_BRIDGES=}" | wc -l))
+fi
+BUNDLE=0
 
 if [ ! -z ${SIM_ENVIRONMENTS+x} ]; then
     getAssets "${SIM_ENVIRONMENTS}"
     for A in ${ASSETS}; do
-        build_bundle Environments ${A} | tee -a unity-build-bundles.log
+        build_bundle Environments ${A} 2>&1 | tee -a unity-build-bundles-Environments.log
     done
 fi
 if [ ! -z ${SIM_VEHICLES+x} ]; then
     getAssets "${SIM_VEHICLES}"
     for A in ${ASSETS}; do
-        build_bundle Vehicles ${A} | tee -a unity-build-bundles.log
+        build_bundle Vehicles ${A} 2>&1 | tee -a unity-build-bundles-Vehicles.log
     done
 fi
 
 if [ ! -z ${SIM_SENSORS+x} ]; then
     getAssets "${SIM_SENSORS}"
     for A in ${ASSETS}; do
-        build_bundle Sensors ${A} | tee -a unity-build-bundles.log
+        build_bundle Sensors ${A} 2>&1 | tee -a unity-build-bundles-Sensors.log
     done
 fi
 
 if [ ! -z ${SIM_BRIDGES+x} ]; then
     getAssets "${SIM_BRIDGES}"
     for A in ${ASSETS}; do
-        build_bundle Bridges ${A} | tee -a unity-build-bundles.log
+        build_bundle Bridges ${A} 2>&1 | tee -a unity-build-bundles-Bridges.log
     done
 fi
 
-check_unity_log unity-build-bundles.log
+RESULT=0
+OK=0
+
+# maybe drop -e before the next section, because "|| true" is needed after
+# each grep which doesn't match anything (including grep -c), and also after
+# expr 0 + 0, which prints 0, but returns 1 as return status
+echo "Build bundles summary:"
+for assetType in Environments Vehicles Sensors Bridges; do
+    if [ -f unity-build-bundles-${assetType}.log ] ; then
+        echo "== ${assetType} built successfully =="
+        grep "^INFO: .* succeeded to build" unity-build-bundles-${assetType}.log || true
+        OK=$(expr ${OK} + $(grep -c "^INFO: .* succeeded to build" unity-build-bundles-${assetType}.log || true) || true)
+        echo "== ${assetType} built with errors =="
+        grep "^ERROR:" unity-build-bundles-${assetType}.log || true
+        RESULT=$(expr ${RESULT} + $(grep -c "^ERROR:" unity-build-bundles-${assetType}.log || true) || true)
+        echo "== ${assetType} built with warnings =="
+        grep "^WARN:" unity-build-bundles-${assetType}.log || true
+        echo "== ${assetType} other possible issues detected by check_unity_log =="
+        Jenkins/check-unity-log.sh unity-build-bundles-${assetType}.log || RESULT=$(expr ${RESULT} + 1)
+    else
+        echo "No ${assetType} were built (unity-build-bundles-${assetType}.log doesn't exist)"
+    fi
+done
+echo "${OK} from ${BUNDLES} bundles were built successfully, there were ${RESULT} issues"
+exit ${RESULT}

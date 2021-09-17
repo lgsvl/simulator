a3e0f95a5da6f10928d41b1635d852c9ef7c084e
Martin Jansa <martin.jansa@lge.com>
2021-09-17 13:30:35 +0200

    Jenkins: remove some unnecessary changes
    * now when Unity doesn't segfault when Library is empty we can remove some work arounds
      which seemd to fix some of the segfault causes
    
    * we still cannot use Xvfb, it still segfaults even when repeating build 3 times and
      keeping Library between the builds
    
    * use latest hdrp/unityci/editor docker image built in:
      https://auto-jenkins.lgsvl.net/view/docker/job/unityci-docker-image/30/

--- Jenkins/build-bundles.sh
+++ Jenkins/build-bundles.sh
@@ -93,7 +93,6 @@ function build_bundle {
     echo "Building bundle (${BUNDLE}/${BUNDLES}) $*"
     if [ ! -d .external-assets/$1/$2 ] ; then
         echo "ERROR: Bundle source doesn't exist in .external-assets/$1/$2"
-        exit 1
     else
         mkdir -p Assets/External/$1
         mv .external-assets/$1/$2 Assets/External/$1
@@ -119,7 +118,6 @@ function build_bundle {
     mv Assets/External/$1/$2 .external-assets/$1/$2
     if [ ! -f AssetBundles/$1/*_$2 ] ; then
         echo "ERROR: Bundle $1 $2 wasn't created in AssetBundles/$1/*_$2"
-        exit 1
     else
         echo "INFO: Bundle $1 $2 succeeded to build"
     fi
@@ -204,6 +202,8 @@ OK=0
 # maybe drop -e before the next section, because "|| true" is needed after
 # each grep which doesn't match anything (including grep -c), and also after
 # expr 0 + 0, which prints 0, but returns 1 as return status
+echo
+echo
 echo "Build bundles summary:"
 for assetType in Environments Vehicles Sensors Bridges; do
     if [ -f unity-build-bundles-${assetType}.log ] ; then
